a1935abf9443e75704e9a379a00c57f16df23fe7
PavelShvets <pasha.shvets99@mail.ru>
2023-06-12 14:50:11 +0300

    97%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,10 +11,13 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3, 4, 5], 0, ), 1)
         self.assertEqual(arrs.get(['cat', 'dog', 'mouse'], 0, "test"), 'cat')
 
+
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4, -1), [5, 6, 7, 8, 9])
         self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5, 6, 7], -1, 3), [])
         self.assertEqual(arrs.my_slice([1, 2, 3, 4, 5]), [1, 2, 3, 4, 5])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 10))
 
